2a6c35b20d00aa9267c122b175d39a119511ec68
Lucas de Ávila Martins <lucas.a.martins@ufv.br>
2018-08-25 15:52:26 -0300

    update template

--- example/lib/generated/ReadRepositoriesQuery.dart
+++ example/lib/generated/ReadRepositoriesQuery.dart
@@ -1,13 +1,18 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 
+/// A repository contains the content for a project.
 class ReadRepositoriesUserRepositoryConnectionRepository {
   ReadRepositoriesUserRepositoryConnectionRepository(this._d);
 
   final Map<String, dynamic> _d;
 
   String get id => this._d["id"];
+
+  /// The name of the repository.
   String get name => this._d["name"];
+
+  /// Returns a boolean indicating whether the viewing user has starred this starrable.
   bool get viewerHasStarred => this._d["viewerHasStarred"];
 
   String toString() {
@@ -15,11 +20,13 @@ class ReadRepositoriesUserRepositoryConnectionRepository {
   }
 }
 
+/// A list of repositories owned by the subject.
 class ReadRepositoriesUserRepositoryConnection {
   ReadRepositoriesUserRepositoryConnection(this._d);
 
   final Map<String, dynamic> _d;
 
+  /// A repository contains the content for a project.
   List<Option<ReadRepositoriesUserRepositoryConnectionRepository>> get nodes =>
       (this._d["nodes"] as List)
           .map<Option<ReadRepositoriesUserRepositoryConnectionRepository>>(
@@ -34,11 +41,13 @@ class ReadRepositoriesUserRepositoryConnection {
   }
 }
 
+/// A user is an individual's account on GitHub that owns repositories and can make new content.
 class ReadRepositoriesUser {
   ReadRepositoriesUser(this._d);
 
   final Map<String, dynamic> _d;
 
+  /// A list of repositories owned by the subject.
   ReadRepositoriesUserRepositoryConnection get repositories =>
       ReadRepositoriesUserRepositoryConnection(this._d["repositories"]);
 
@@ -52,5 +61,10 @@ class ReadRepositories {
 
   final Map<String, dynamic> _d;
 
+  /// A user is an individual's account on GitHub that owns repositories and can make new content.
   ReadRepositoriesUser get viewer => ReadRepositoriesUser(this._d["viewer"]);
+
+  String toString() {
+    return this._d.toString();
+  }
 }
